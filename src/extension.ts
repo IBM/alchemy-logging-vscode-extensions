51fb4d36f64487aa319cf806924bca0832dc01fc
Gaurav-Kumbhat <Gaurav.Kumbhat@ibm.com>
2022-05-07 19:21:37 -0400

    :rotating_light: Add linter and fix formatting

--- src/extension.ts
+++ src/extension.ts
@@ -23,16 +23,16 @@ const logFirstRegex = new RegExp("^log\.([a-z]{4,7}[1-5]?)\\(\"[A-Z]{3}", "gs");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext): void {
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "alog-code-generator" is now active!');
+  // This line of code will only be executed once when your extension is activated
+  // console.log('Congratulations, your extension "alog-code-generator" is now active!');
 
-	function registerCommandNice(commandId: string, run: (...args: any[]) => void): void {
-		context.subscriptions.push(vscode.commands.registerCommand(commandId, run));
-	}
+  function registerCommandNice(commandId: string, run: (...args: any[]) => void): void {
+	  context.subscriptions.push(vscode.commands.registerCommand(commandId, run));
+  }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
 	// registerCommandNice("alog-code-generator.log_code", () => {
 	// 	vscode.window.showInformationMessage("");
 	// });
@@ -43,21 +43,23 @@ export function activate(context: vscode.ExtensionContext): void {
 		const activeEditor = vscode.window.activeTextEditor;
 		const searchWindow = 10;
 		const startPosition: number = 0;
-		var lineNumber: number;
-		var rawLineText: string;
-		var activePosition: number;
-		var logCodeSuffix: string | undefined;
+		let lineNumber: number;
+		let rawLineText: string;
+		let activePosition: number;
+		let logCodeSuffix: string | undefined;
 
 		if (activeEditor) {
 			lineNumber = activeEditor.selection.active.line;
 			activePosition = activeEditor.selection.active.character;
 			rawLineText = activeEditor.document.lineAt(activeEditor.selection.active.line).text.trimStart();
 
-			const levelPatternMatch: RegExpMatchArray | null  = errorFirstRegex.exec(rawLineText) || logFirstRegex.exec(rawLineText);
-			if (levelPatternMatch != null && levelPatternMatch?.length > 0) {
+			const levelPatternMatch: RegExpMatchArray | null =
+				errorFirstRegex.exec(rawLineText) || logFirstRegex.exec(rawLineText);
+
+			if (levelPatternMatch !== null && levelPatternMatch?.length > 0) {
 				const matchedCapture = levelPatternMatch[1];
-				const logCodeSuffix = constants.pythonLogKeywords.get(matchedCapture);
-				if (logCodeSuffix != undefined) {
+				logCodeSuffix = constants.pythonLogKeywords.get(matchedCapture);
+				if (logCodeSuffix !== undefined) {
 					const logCodeNumber = generate(digitCount);
 					const suggestedLogCode = logCodeNumber + logCodeSuffix;
 					activeEditor.edit(editBuilder => {
@@ -76,19 +78,18 @@ export function activate(context: vscode.ExtensionContext): void {
 	// );
 	// context.subscriptions.push(disposable, autoSuggestor);
 
-
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
 
 // Log code generator function
 async function provideCompletionItems(
 	document: vscode.TextDocument,
 	position: vscode.Position,
 	token?: vscode.CancellationToken,
-	context?: vscode.CompletionContext
-) {
+	context?: vscode.CompletionContext) {
+
 	const offset = document.offsetAt(position);
 	const beforeStartOffset = Math.max(0, offset - vscodeConfig.charLimit);
 	const afterEndOffset = offset + 5;
@@ -117,15 +118,17 @@ async function provideCompletionItems(
 }
 
 function generate(n: number): string {
-	var add = 1, max = 12 - add;   // 12 is the min safe number Math.random() can generate without it starting to pad the end with zeros.
+	// 12 is the min safe number Math.random() can generate without it starting to pad the end with zeros.
+	const add = 1;
+	let max = 12 - add;
 
-	if ( n > max ) {
-			return generate(max) + generate(n - max);
+	if (n > max) {
+		return generate(max) + generate(n - max);
 	}
 
-	max        = Math.pow(10, n + add);
-	var min    = max/10; // Math.pow(10, n) basically
-	var number = Math.floor( Math.random() * (max - min + 1) ) + min;
+	max = Math.pow(10, n + add);
+	const min = max / 10; // Math.pow(10, n) basically
+	const genNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 
-	return ("" + number).substring(add);
+	return ("" + genNumber).substring(add);
 }
